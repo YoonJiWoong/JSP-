0c83094060bcc3f3dfd58db977813cd0c936ba85
MyHome <MyHome@DESKTOP-H9O1HL9>
2022-03-31 22:31:12 +0900

    환경구축(이클립스, My SQL),JDBC 로그인 디자인 페이지 만듬 로그인 기능 만듬 회원가입 디자인 페이지 만듬 회원가입 기능 만듬

---
+++ src/main/java/User/UserDAO.java
@@ -0,0 +1,82 @@
+package User;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+
+public class UserDAO {
+	
+	private Connection conn;
+	private PreparedStatement pstmt;
+	private ResultSet rs;
+	
+	public UserDAO() {
+		try {
+			String dbURL = "jdbc:mysql://localhost:3306/BBS?characterEncoding=UTF-8&serverTimezone=Asia/Seoul";
+			String dbID = "root";
+			String dbPassword = "root";
+			Class.forName("com.mysql.cj.jdbc.Driver");
+			conn = DriverManager.getConnection(dbURL, dbID, dbPassword);
+			System.out.println("db접속 성공");
+		} catch (Exception e) {
+			e.printStackTrace();
+			System.out.println("db접속 실패");
+		}		
+	}
+	
+	
+	public int login(String userID, String userPassword) {
+		
+		String SQL = "SELECT userPassword FROM USER WHERE userID=?";
+		
+		try {
+			System.out.println(SQL+" : ");
+			pstmt = conn.prepareStatement(SQL);
+			pstmt.setString(1, userID);
+			rs = pstmt.executeQuery();
+			if(rs.next()) {
+				if(rs.getString(1).equals(userPassword)) {
+					return 1; // 로그인 성공
+				}else {
+					return 0; // 비밀번호 불 일치
+				}
+				
+			}
+			
+			return -1; // 아이디 없음
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return -2; // 데이터베이스 오류 
+	
+	}
+	
+	public int join(User user) {
+		
+		String SQL = "INSERT INTO USER VALUES(?,?,?,?,?)";
+		
+		try {
+			
+			pstmt = conn.prepareStatement(SQL);
+			pstmt.setString(1, user.getUserID());
+			pstmt.setString(2, user.getUserPassword());
+			pstmt.setString(3, user.getUserName());
+			pstmt.setString(4, user.getUserGender());
+			pstmt.setString(5, user.getUserEmail());
+	
+			return pstmt.executeUpdate();
+			
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+			
+		}
+		return -1; // 데이터베이스 오류 
+
+	}
+	
+	
+
+}
